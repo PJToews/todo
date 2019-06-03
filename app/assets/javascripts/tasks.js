390159b3a1aed3bba2733bfc37b92898c10efd68
PJToews <lohasjean@gmail.com>
2019-06-03 19:59:02 +0000

    Polish the Application Design

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -58,7 +58,10 @@ $(function() {
         var ulTodos = $('.todo-list');
         ulTodos.append(htmlString);
         $('.toggle').click(toggleTask);
+        $('.new-todo').val('');
       });
     });
 
   });
+
+
